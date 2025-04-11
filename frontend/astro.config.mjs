45697d59615bec652f1c421d83ba22d4fb7ffb00
idke64 <deliciousbrain42@gmail.com>
2025-04-11 15:49:06 -0400

    Revert "Merge branch 'main' into rv-llm"
    This reverts commit 7881b1372989cfbc852852ac6c8ce49bbbb6d5c3, reversing
    changes made to e776319e4a54fe5452d461083c785604c477b260.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,21 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "node:path";
 
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@components": path.resolve("./src/components"),
-        "@layouts": path.resolve("./src/layouts"),
-        "@styles": path.resolve("./src/styles"),
-        "@pages": path.resolve("./src/pages"),
-      },
-    },
-  },
+    plugins: [tailwindcss()]
+  }
 });
